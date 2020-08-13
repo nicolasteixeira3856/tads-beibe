d2eff04fceeba96cfd9b63d7e3816ef326492712
Nícolas Teixeira <nicolasteixeira3856@outlook.com>
2020-08-13 20:59:33 -0300

    Finalizando a bean de atendimento

---
+++ projeto-beibe/src/java/com/br/beibe/tads/bean/Atendimento.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.beibe.tads.bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nicol
+ */
+public class Atendimento {
+    private int id;
+    private Date dataCriacao;
+    private Usuario $idUsuario;
+}
