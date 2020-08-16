c28f2b08ef4826adebef092cfdf93620532a284d
Nícolas Teixeira <nicolasteixeira3856@outlook.com>
2020-08-15 23:17:11 -0300

    Fix SQL

--- beibe.sql
+++ beibe.sql
@@ -39,43 +39,25 @@ CREATE TABLE `atendimento` (
   KEY `idfk_produto` (`idfk_produto`),
   KEY `idfk_tipo_atendimento` (`idfk_tipo_atendimento`),
   CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`idfk_usuario`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `atendimento_ibfk_2` FOREIGN KEY (`idfk_status`) REFERENCES `atendimento_stauts` (`id`),
+  CONSTRAINT `atendimento_ibfk_2` FOREIGN KEY (`idfk_status`) REFERENCES `atendimento_status` (`id`),
   CONSTRAINT `atendimento_ibfk_3` FOREIGN KEY (`idfk_produto`) REFERENCES `produto` (`id`),
   CONSTRAINT `atendimento_ibfk_4` FOREIGN KEY (`idfk_tipo_atendimento`) REFERENCES `tipo_atendimento` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `atendimento`
+-- Table structure for table `atendimento_status`
 --
 
-LOCK TABLES `atendimento` WRITE;
-/*!40000 ALTER TABLE `atendimento` DISABLE KEYS */;
-/*!40000 ALTER TABLE `atendimento` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `atendimento_stauts`
---
-
-DROP TABLE IF EXISTS `atendimento_stauts`;
+DROP TABLE IF EXISTS `atendimento_status`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `atendimento_stauts` (
+CREATE TABLE `atendimento_status` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `atendimento_stauts`
---
-
-LOCK TABLES `atendimento_stauts` WRITE;
-/*!40000 ALTER TABLE `atendimento_stauts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `atendimento_stauts` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `categoria_produto`
@@ -90,16 +72,6 @@ CREATE TABLE `categoria_produto` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `categoria_produto`
---
-
-LOCK TABLES `categoria_produto` WRITE;
-/*!40000 ALTER TABLE `categoria_produto` DISABLE KEYS */;
-INSERT INTO `categoria_produto` VALUES (1,'Higiene pessoal');
-/*!40000 ALTER TABLE `categoria_produto` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `cidade`
@@ -117,16 +89,6 @@ CREATE TABLE `cidade` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cidade`
---
-
-LOCK TABLES `cidade` WRITE;
-/*!40000 ALTER TABLE `cidade` DISABLE KEYS */;
-INSERT INTO `cidade` VALUES (1,1,'Curitiba');
-/*!40000 ALTER TABLE `cidade` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `estado`
 --
 
@@ -139,16 +101,6 @@ CREATE TABLE `estado` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `estado`
---
-
-LOCK TABLES `estado` WRITE;
-/*!40000 ALTER TABLE `estado` DISABLE KEYS */;
-INSERT INTO `estado` VALUES (1,'PR');
-/*!40000 ALTER TABLE `estado` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `produto`
@@ -170,16 +122,6 @@ CREATE TABLE `produto` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `produto`
---
-
-LOCK TABLES `produto` WRITE;
-/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'Condicionador Dona Bella',1,'Condicionador para cabelos meia idade.','200');
-/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tipo_atendimento`
 --
 
@@ -192,16 +134,6 @@ CREATE TABLE `tipo_atendimento` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tipo_atendimento`
---
-
-LOCK TABLES `tipo_atendimento` WRITE;
-/*!40000 ALTER TABLE `tipo_atendimento` DISABLE KEYS */;
-INSERT INTO `tipo_atendimento` VALUES (1,'Problema com o produto');
-/*!40000 ALTER TABLE `tipo_atendimento` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -227,16 +159,6 @@ CREATE TABLE `usuario` (
   CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idfk_cidade`) REFERENCES `cidade` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Nicolas Teixeira','11167328906','nicolasteixeira3856@outlook.com','Rua Maria da Luz Rocha Belão, 610',1,'41999409631','8e406feab23e1239a50b0d329b0754fb',3);
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -247,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-15  1:38:34
+-- Dump completed on 2020-08-15 23:16:45
